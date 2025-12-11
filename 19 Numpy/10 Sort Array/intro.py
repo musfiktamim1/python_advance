e11b7e60be896c980811a667458113ccf1e7a14c
musfiktamim1 <musfiktamim2@gmail.com>
2025-12-11 19:53:50 +0000

    sorting

---
+++ 19 Numpy/10 Sort Array/intro.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+arr = np.array([2,1,4,3,6,5])
+print(np.sort(arr))
+
+
+arr = np.array(['banana', 'cherry', 'apple'])
+
+print(np.sort(arr))
+
+
+arr = np.array([True, False, True])
+
+print(np.sort(arr))
+
+
+
+arr = np.array([[3, 2, 4], [5, 0, 1]])
+
+print(np.sort(arr))
